5b30551979d00980dd19b28582194229cca97c85
lfigueroa <laurofigueroa@hotmail.com>
2019-11-23 12:56:31 -0300

    working games interface

--- tp_final_2016/games.erl
+++ tp_final_2016/games.erl
@@ -2,19 +2,18 @@
 -compile(export_all).
 -include("game_interface.hrl").
 
-
 games(Game_list) ->
     receive
-    {From, {add, Game}} ->
-        From ! {self(), ok},
-        games([Game | Game_list]);
-    {From, {get, Game_id}} ->
-        Game = #game{id=Game_id, state=[]},
-        From ! {self(), Game},
-        games(Game_list);
-    {From, get_all_games} ->
-        From ! {self(), Game_list},
-        games(Game_list)
+	{From, {add, Game}} ->
+	    From ! {self(), ok},
+	    games([Game | Game_list]);
+	{From, {get, Game_id}} ->
+	    Game = #game{id=Game_id, state=[]},
+	    From ! {self(), Game},
+	    games(Game_list);
+	{From, get_all_games} ->
+	    From ! {self(), Game_list},
+	    games(Game_list)
     end.
 
 get(Pid, Game_id) ->
@@ -26,11 +25,11 @@ get(Pid, Game_id) ->
 get_all(Pid) ->
     Pid ! {self(), get_all_games},
     receive
-           {_, Games} -> Games
+	{_, Games} -> Games
     end.
 
 add(Pid, Game) ->
     Pid ! {self(), {add, Game}},
     receive
-           {_, ok} -> ok
+	{_, ok} -> ok
     end.
